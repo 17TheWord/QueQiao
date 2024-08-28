6457b1ea3a4212131c8e9d458bdb65885711b284
17TheWord <17theword@gmail.com>
2024-08-28 21:26:33 +0800

    refactor(handle-api): 重命名消息处理方法以区分发送动作为HandleApiImpl中的方法重命名，以更好地区分消息发送和处理操作。修改后的命名约定现在明确指示“发送”操作，例如`handleSendPrivateMessage`和`handleSendActionBarMessage`，提升代码可读性并减少混淆可能性。

--- velocity/origin/src/main/java/com/github/theword/queqiao/handle/HandleApiImpl.java
+++ velocity/origin/src/main/java/com/github/theword/queqiao/handle/HandleApiImpl.java
@@ -3,31 +3,32 @@ package com.github.theword.queqiao.handle;
 import com.github.theword.queqiao.tool.handle.HandleApiService;
 import com.github.theword.queqiao.tool.payload.MessageSegment;
 import com.github.theword.queqiao.tool.payload.TitlePayload;
+import com.github.theword.queqiao.tool.response.PrivateMessageResponse;
 import com.github.theword.queqiao.tool.utils.Tool;
 import com.github.theword.queqiao.utils.ParseJsonToEventImpl;
 import com.velocitypowered.api.proxy.Player;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.title.TitlePart;
-import org.java_websocket.WebSocket;
 
 import java.util.List;
 import java.util.UUID;
 
 import static com.github.theword.queqiao.QueQiao.minecraftServer;
+import static com.github.theword.queqiao.utils.VelocityTool.getVelocityPlayer;
 
 public class HandleApiImpl implements HandleApiService {
 
     private final ParseJsonToEventImpl parseJsonToEvent = new ParseJsonToEventImpl();
 
     @Override
-    public void handleBroadcastMessage(WebSocket webSocket, List<MessageSegment> messageList) {
+    public void handleBroadcastMessage(List<MessageSegment> messageList) {
         Component component = parseJsonToEvent.parsePerMessageToComponent(Tool.getPrefixComponent());
         Component append = component.append(parseJsonToEvent.parseMessageListToComponent(messageList));
         minecraftServer.sendMessage(append);
     }
 
     @Override
-    public void handleSendTitleMessage(WebSocket webSocket, TitlePayload titlePayload) {
+    public void handleSendTitleMessage(TitlePayload titlePayload) {
         Component title = parseJsonToEvent.parseMessageListToComponent(titlePayload.getTitle());
         minecraftServer.sendTitlePart(TitlePart.TITLE, title);
         if (titlePayload.getSubtitle() != null) {
@@ -37,21 +38,20 @@ public class HandleApiImpl implements HandleApiService {
     }
 
     @Override
-    public void handleActionBarMessage(WebSocket webSocket, List<MessageSegment> messageList) {
+    public void handleSendActionBarMessage(List<MessageSegment> messageList) {
         Component component = parseJsonToEvent.parsePerMessageToComponent(Tool.getPrefixComponent());
         minecraftServer.sendActionBar(component);
     }
 
     @Override
-    public void handlePrivateMessage(WebSocket webSocket, String targetPlayerName, UUID targetPlayerUuid, List<MessageSegment> messageList) {
+    public PrivateMessageResponse handleSendPrivateMessage(String nickname, UUID uuid, List<MessageSegment> messageList) {
         for (Player player : minecraftServer.getAllPlayers()) {
-            if ((targetPlayerUuid != null && targetPlayerUuid.equals(player.getUniqueId())) || (targetPlayerName != null && targetPlayerName.equals(player.getUsername()))) {
+            if ((uuid != null && uuid.equals(player.getUniqueId())) || (nickname != null && nickname.equals(player.getUsername()))) {
                 Component component = parseJsonToEvent.parsePerMessageToComponent(Tool.getPrefixComponent());
                 minecraftServer.sendMessage(component);
-//                webSocket.send("{ \"result\": 200, \"message\": \"Message sent\" }");
-                break;
+                return PrivateMessageResponse.sendSuccess(getVelocityPlayer(player));
             }
         }
-//        webSocket.send("{ \"result\": 404, \"message\": \"Player not found\" }");
+        return PrivateMessageResponse.playerNotFound();
     }
 }
