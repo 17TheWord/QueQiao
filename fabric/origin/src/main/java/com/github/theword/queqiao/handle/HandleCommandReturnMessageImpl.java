8b7f975263348bbd0070b30f4c6819f497c0a6b0
17TheWord <17theword@gmail.com>
2024-09-11 11:08:49 +0800

    :sparkles: Add permission check for command

--- fabric/origin/src/main/java/com/github/theword/queqiao/handle/HandleCommandReturnMessageImpl.java
+++ fabric/origin/src/main/java/com/github/theword/queqiao/handle/HandleCommandReturnMessageImpl.java
@@ -1,5 +1,6 @@
 package com.github.theword.queqiao.handle;
 
+import com.github.theword.queqiao.tool.constant.BaseConstant;
 import com.github.theword.queqiao.tool.handle.HandleCommandReturnMessageService;
 import com.mojang.brigadier.context.CommandContext;
 import net.minecraft.server.command.ServerCommandSource;
@@ -18,9 +19,26 @@ public class HandleCommandReturnMessageImpl implements HandleCommandReturnMessag
         CommandContext<ServerCommandSource> context = (CommandContext<ServerCommandSource>) object;
         if (context.getSource().getEntity() instanceof ServerPlayerEntity)
             // IF >= fabric-1.20
-//          context.getSource().sendFeedback(() -> Text.of(message), false);
-            // ELSE
+//            context.getSource().sendFeedback(() -> Text.of(message), false);
+        // ELSE
 //            context.getSource().sendFeedback(Text.of(message), false);
         // END IF
+    }
+
+    /**
+     * 判断发送者是否有权执行命令
+     * <p>MOD端中无权限节点，权限等级为2</p>
+     *
+     * @param object 命令返回者
+     * @param node   权限节点
+     * @return boolean 是否有权限
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    public boolean hasPermission(Object object, String node) {
+        CommandContext<ServerCommandSource> context = (CommandContext<ServerCommandSource>) object;
+        if (context.getSource().hasPermissionLevel(BaseConstant.MOD_PERMISSION_LEVEL)) return true;
+        handleCommandReturnMessage(object, "你没有权限执行此命令");
+        return false;
     }
 }
