38fc8662fee4f4262e2c490f58d7e68bc11325d2
17TheWord <17theword@gmail.com>
2025-02-14 21:23:15 +0800

    :construction_worker: Rollback

--- matrix.ps1
+++ matrix.ps1
@@ -1,28 +1,3 @@
-#param (
-#    [string[]]$paths
-#)
-#
-#$allFolderObjects = @()
-#
-#foreach ($path in $paths) {
-#    $folders = Get-ChildItem -Path $path -Directory
-#    $filteredFolders = $folders | Where-Object { $_.Name -ne "origin" }
-#
-#    $folderObjects = $filteredFolders | ForEach-Object {
-#        [PSCustomObject]@{
-#            "mc-version" = $_.Name.Replace("$path-", "")
-#            "mc-loader"  = $path
-#        }
-#    }
-#
-#    $allFolderObjects += $folderObjects
-#}
-#
-#$json = [PSCustomObject]@{
-#    "config" = $allFolderObjects
-#} | ConvertTo-Json -Compress
-#
-#Write-Output "::set-output name=matrix::$json"
 param (
     [string[]]$paths
 )
@@ -47,9 +22,4 @@ $json = [PSCustomObject]@{
     "config" = $allFolderObjects
 } | ConvertTo-Json -Compress
 
-# 使用环境文件输出替换 `set-output`
-$env:GITHUB_ENV = "$env:GITHUB_WORKSPACE/.github/environment"
-$json | Out-File -Append -Encoding utf8 -FilePath $env:GITHUB_ENV
-# 添加调试输出，查看 JSON 内容
-Write-Host "Generated JSON: $json"
-
+Write-Output "::set-output name=matrix::$json"
