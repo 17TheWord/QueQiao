67b722cf103ab6798cc255b00b8ee246ee60ed60
17TheWord <17theword@gmail.com>
2025-03-13 20:43:33 +0800

    :white_check_mark: Add parse ChatColor test

---
+++ spigot/origin/src/test/java/com/github/theword/queqiao/utils/ParseJsonToEventImplTest.java
@@ -0,0 +1,18 @@
+package com.github.theword.queqiao.utils;
+
+import org.junit.jupiter.api.Test;
+import net.md_5.bungee.api.ChatColor;
+
+class ParseJsonToEventImplTest {
+
+    @Test
+    void parseChatColor() {
+        // IF spigot-1.12.2
+//        ChatColor yellow = ChatColor.valueOf("BLACK");
+        // ELSE
+//        ChatColor yellow = ChatColor.of("BLACK");
+        // END IF
+        System.out.println("yellow = " + yellow);
+    }
+
+}
