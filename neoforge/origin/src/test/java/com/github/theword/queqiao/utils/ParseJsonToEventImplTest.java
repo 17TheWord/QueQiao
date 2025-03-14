fd075483d9420cf4e9803698579562bb4b5f91c8
17TheWord <17theword@gmail.com>
2025-03-14 12:00:05 +0800

    :white_check_mark: Add unit tests for json message processing

--- neoforge/origin/src/test/java/com/github/theword/queqiao/utils/ParseJsonToEventImplTest.java
+++ neoforge/origin/src/test/java/com/github/theword/queqiao/utils/ParseJsonToEventImplTest.java
@@ -4,7 +4,6 @@ import com.github.theword.queqiao.tool.response.Response;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import org.junit.jupiter.api.Test;
-import net.md_5.bungee.api.ChatColor;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -13,29 +12,14 @@ import static com.github.theword.queqiao.tool.utils.TestUtils.testParseJsonMessa
 import static com.github.theword.queqiao.tool.utils.Tool.logger;
 import static org.slf4j.LoggerFactory.getLogger;
 
-
 class ParseJsonToEventImplTest {
-
-    @Test
-    void parseChatColor() {
-        // IF spigot-1.12.2
-//        ChatColor yellow = ChatColor.valueOf("BLACK");
-        // ELSE
-//        ChatColor yellow = ChatColor.of("BLACK");
-        // END IF
-        System.out.println("yellow = " + yellow);
-    }
-
     ParseJsonToEventImpl parseJsonToEventImpl = new ParseJsonToEventImpl();
 
     @Test
     void parseMessage() throws FileNotFoundException {
         FileReader reader = new FileReader("../../test_messages.json");
-        // IF spigot-1.12.2
-//        JsonElement testMessageJsonElement = new JsonParser().parse(reader);
-        // ELSE
-//        JsonElement testMessageJsonElement = JsonParser.parseReader(reader);
-        // END IF
+        JsonElement testMessageJsonElement = JsonParser.parseReader(reader);
+
         logger = getLogger(getClass());
 
         if (!testMessageJsonElement.isJsonArray()) {
