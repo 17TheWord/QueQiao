e136c2b968e14d3aa0ac9c5e8328bb95d362d1d4
Blue-Beaker <yyy5566778899@126.com>
2025-05-18 01:09:55 +0800

    Add missing player properties

--- src/main/java/com/github/theword/queqiao/utils/ForgeTool.java
+++ src/main/java/com/github/theword/queqiao/utils/ForgeTool.java
@@ -1,12 +1,33 @@
 package com.github.theword.queqiao.utils;
 
 import com.github.theword.queqiao.event.forge.ForgeServerPlayer;
+import net.minecraft.entity.ai.attributes.AttributeMap;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.math.BlockPos;
 
 public class ForgeTool {
     public static ForgeServerPlayer getForgePlayer(EntityPlayerMP player) {
         ForgeServerPlayer forgeServerPlayer = new ForgeServerPlayer();
+
+        forgeServerPlayer.setNickname(player.getName());
         forgeServerPlayer.setDisplayName(player.getDisplayNameString());
+
+        forgeServerPlayer.setUuid(player.getGameProfile().getId());
+        forgeServerPlayer.setIpAddress(player.getPlayerIP());
+
+        forgeServerPlayer.setSpeed(player.capabilities.getWalkSpeed());
+        forgeServerPlayer.setFlyingSpeed(player.capabilities.getFlySpeed());
+
+        forgeServerPlayer.setGameMode(player.interactionManager.getGameType().getName());
+        forgeServerPlayer.setFlying(player.capabilities.isFlying);
+        forgeServerPlayer.setSleeping(player.isPlayerSleeping());
+        forgeServerPlayer.setBlocking(player.isActiveItemStackBlocking());
+
+        BlockPos position = player.getPosition();
+        forgeServerPlayer.setBlockX(position.getX());
+        forgeServerPlayer.setBlockY(position.getY());
+        forgeServerPlayer.setBlockZ(position.getZ());
+
         return forgeServerPlayer;
     }
 }
