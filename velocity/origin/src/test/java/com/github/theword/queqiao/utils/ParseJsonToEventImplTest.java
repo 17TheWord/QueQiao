7932bd894beaf6e83c37201c276b836d56c4d5f3
17TheWord <17theword@gmail.com>
2025-09-25 14:15:51 +0800

    :white_check_mark: Improve logging in ParseJsonToEventImplTest and enhance message formatting

--- velocity/origin/src/test/java/com/github/theword/queqiao/utils/ParseJsonToEventImplTest.java
+++ velocity/origin/src/test/java/com/github/theword/queqiao/utils/ParseJsonToEventImplTest.java
@@ -4,23 +4,25 @@ import com.github.theword.queqiao.tool.response.Response;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 import static com.github.theword.queqiao.tool.utils.TestUtils.testParseJsonMessage;
-import static com.github.theword.queqiao.tool.utils.Tool.logger;
-import static org.slf4j.LoggerFactory.getLogger;
+
 
 class ParseJsonToEventImplTest {
 
-    ParseJsonToEventImpl parseJsonToEventImpl = new ParseJsonToEventImpl();
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private final ParseJsonToEventImpl parseJsonToEventImpl = new ParseJsonToEventImpl();
 
     @Test
     void parseMessage() throws FileNotFoundException {
         FileReader reader = new FileReader("../../test_messages.json");
         JsonElement testMessageJsonElement = JsonParser.parseReader(reader);
-        logger = getLogger(getClass());
 
         if (!testMessageJsonElement.isJsonArray()) {
             logger.warn("jsonElement is not json array");
@@ -32,7 +34,7 @@ class ParseJsonToEventImplTest {
         for (JsonElement message : testMessageJsonElement.getAsJsonArray()) {
             logger.info("============================================");
             Response response = testParseJsonMessage(message.getAsString(), parseJsonToEventImpl);
-            logger.info("response = " + response);
+            logger.info("response = {}", response);
         }
 
     }
