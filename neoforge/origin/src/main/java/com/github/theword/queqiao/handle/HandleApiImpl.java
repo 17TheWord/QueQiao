c8fbebf342606764775aa6bb7489e75d1d5b8332
17TheWord <17theword@gmail.com>
2025-10-13 23:59:26 +0800

    :sparkles: 适配新版Json to Component

--- neoforge/origin/src/main/java/com/github/theword/queqiao/handle/HandleApiImpl.java
+++ neoforge/origin/src/main/java/com/github/theword/queqiao/handle/HandleApiImpl.java
@@ -1,17 +1,15 @@
 package com.github.theword.queqiao.handle;
 
 import com.github.theword.queqiao.tool.handle.HandleApiService;
-import com.github.theword.queqiao.tool.payload.MessageSegment;
-import com.github.theword.queqiao.tool.payload.TitlePayload;
 import com.github.theword.queqiao.tool.response.PrivateMessageResponse;
 import com.github.theword.queqiao.tool.utils.Tool;
-import com.github.theword.queqiao.utils.ParseJsonToEventImpl;
+import com.google.gson.JsonElement;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.*;
 import net.minecraft.server.level.ServerPlayer;
 
-import java.util.List;
 import java.util.UUID;
 
 import static com.github.theword.queqiao.Queqiao.minecraftServer;
@@ -19,42 +17,56 @@ import static com.github.theword.queqiao.utils.NeoForgeTool.getNeoForgePlayer;
 
 
 public class HandleApiImpl implements HandleApiService {
-    private final ParseJsonToEventImpl parseJsonToEventImpl = new ParseJsonToEventImpl();
 
     /**
      * 广播消息
      *
-     * @param messageList message
+     * @param jsonElement message
      */
     @Override
-    public void handleBroadcastMessage(List<MessageSegment> messageList) {
-        MutableComponent mutableComponent = parseJsonToEventImpl.parsePerMessageToComponent(Tool.getPrefixComponent());
-
-        mutableComponent.append(parseJsonToEventImpl.parseMessageListToComponent(messageList));
+    public void handleBroadcastMessage(JsonElement jsonElement) {
+        MutableComponent mutableComponent = Component.Serializer.fromJson(Tool.getPrefixComponent(), minecraftServer.registryAccess());
+        assert mutableComponent != null;
+        Component message = Component.Serializer.fromJson(jsonElement, minecraftServer.registryAccess());
+        if (message != null) {
+            mutableComponent = mutableComponent.copy();
+            mutableComponent.append(message);
+        }
         sendPacket(new ClientboundSystemChatPacket(mutableComponent, false));
     }
 
     /**
      * 广播 Send Title 消息
      *
-     * @param titlePayload Send Title message
+     * @param titleJsonElement    title message
+     * @param subtitleJsonElement subtitle message
+     * @param fadein              fadein time
+     * @param stay                stay time
+     * @param fadeout             fadeout time
      */
     @Override
-    public void handleSendTitleMessage(TitlePayload titlePayload) {
-        sendPacket(new ClientboundSetTitleTextPacket(parseJsonToEventImpl.parseMessageListToComponent(titlePayload.getTitle())));
-        if (titlePayload.getSubtitle() != null)
-            sendPacket(new ClientboundSetSubtitleTextPacket(parseJsonToEventImpl.parseMessageListToComponent(titlePayload.getSubtitle())));
-        sendPacket(new ClientboundSetTitlesAnimationPacket(titlePayload.getFadein(), titlePayload.getStay(), titlePayload.getFadeout()));
+    public void handleSendTitleMessage(JsonElement titleJsonElement, JsonElement subtitleJsonElement, int fadein, int stay, int fadeout) {
+        sendPacket(new ClientboundSetTitlesAnimationPacket(fadein, stay, fadeout));
+        MutableComponent title = Component.Serializer.fromJson(titleJsonElement, minecraftServer.registryAccess());
+        if (title != null) {
+            sendPacket(new ClientboundSetTitleTextPacket(title));
+        }
+        MutableComponent subtitle = Component.Serializer.fromJson(subtitleJsonElement, minecraftServer.registryAccess());
+        if (subtitle != null) {
+            sendPacket(new ClientboundSetSubtitleTextPacket(subtitle));
+        }
     }
 
     /**
      * 广播 Action Bar 消息
      *
-     * @param messageList Action Bar message
+     * @param jsonElement Action Bar message
      */
     @Override
-    public void handleSendActionBarMessage(List<MessageSegment> messageList) {
-        sendPacket(new ClientboundSetActionBarTextPacket(parseJsonToEventImpl.parseMessageListToComponent(messageList)));
+    public void handleSendActionBarMessage(JsonElement jsonElement) {
+        MutableComponent mutableComponent = Component.Serializer.fromJson(jsonElement, minecraftServer.registryAccess());
+        if (mutableComponent == null) return;
+        sendPacket(new ClientboundSetActionBarTextPacket(mutableComponent));
     }
 
     /**
@@ -62,10 +74,11 @@ public class HandleApiImpl implements HandleApiService {
      *
      * @param nickname    目标玩家名称
      * @param uuid        目标玩家 UUID
-     * @param messageList message
+     * @param jsonElement message
+     * @return PrivateMessageResponse
      */
     @Override
-    public PrivateMessageResponse handleSendPrivateMessage(String nickname, UUID uuid, List<MessageSegment> messageList) {
+    public PrivateMessageResponse handleSendPrivateMessage(String nickname, UUID uuid, JsonElement jsonElement) {
         ServerPlayer targetPlayer;
         if (uuid != null)
             targetPlayer = minecraftServer.getPlayerList().getPlayer(uuid);
@@ -82,9 +95,13 @@ public class HandleApiImpl implements HandleApiService {
         if (targetPlayer.hasDisconnected()) {
             return PrivateMessageResponse.playerNotOnline();
         }
-        MutableComponent mutableComponent = parseJsonToEventImpl.parsePerMessageToComponent(Tool.getPrefixComponent());
-        mutableComponent.append(parseJsonToEventImpl.parseMessageListToComponent(messageList));
-
+        MutableComponent mutableComponent = Component.Serializer.fromJson(Tool.getPrefixComponent(), minecraftServer.registryAccess());
+        assert mutableComponent != null;
+        MutableComponent message = Component.Serializer.fromJson(jsonElement, minecraftServer.registryAccess());
+        if (message != null) {
+            mutableComponent = mutableComponent.copy();
+            mutableComponent.append(message);
+        }
         targetPlayer.sendSystemMessage(mutableComponent);
 
         return PrivateMessageResponse.sendSuccess(getNeoForgePlayer(targetPlayer));
--- neoforge/origin/src/test/java/com/github/theword/queqiao/utils/ParseJsonToEventImplTest.java
+++
@@ -1,6 +0,0 @@
-package com.github.theword.queqiao.utils;
-
-
-class ParseJsonToEventImplTest {
-
-}
