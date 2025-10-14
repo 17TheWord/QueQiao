348b2d22c19d88e6ad85d23393f64ea0102d15d1
17TheWord <17theword@gmail.com>
2025-10-14 11:34:43 +0800

    :sparkles: 适配新的工具包接口

--- minecraft/origin/src/main/kotlin/com/github/theword/queqiao/handle/HandleApiImpl.kt
+++ minecraft/origin/src/main/kotlin/com/github/theword/queqiao/handle/HandleApiImpl.kt
@@ -1,23 +1,28 @@
 package com.github.theword.queqiao.handle
 
 import com.github.theword.queqiao.tool.handle.HandleApiService
-import com.github.theword.queqiao.tool.payload.MessageSegment
-import com.github.theword.queqiao.tool.payload.TitlePayload
 import com.github.theword.queqiao.tool.response.PrivateMessageResponse
+import com.google.gson.JsonElement
 import java.util.UUID
 
 
 class HandleApiImpl : HandleApiService {
-    override fun handleBroadcastMessage(list: List<MessageSegment>) {
+    override fun handleBroadcastMessage(jsonElement: JsonElement) {
     }
 
-    override fun handleSendTitleMessage(titlePayload: TitlePayload) {
+    override fun handleSendTitleMessage(
+        titleElement: JsonElement,
+        subtitleElement: JsonElement,
+        fadein: Int,
+        stay: Int,
+        fadeout: Int,
+    ) {
     }
 
-    override fun handleSendActionBarMessage(list: List<MessageSegment>) {
+    override fun handleSendActionBarMessage(jsonElement: JsonElement) {
     }
 
-    override fun handleSendPrivateMessage(s: String, uuid: UUID, list: List<MessageSegment>): PrivateMessageResponse {
+    override fun handleSendPrivateMessage(s: String, uuid: UUID, jsonElement: JsonElement): PrivateMessageResponse {
         return PrivateMessageResponse.of(null, "该接口不可用")
     }
 }
