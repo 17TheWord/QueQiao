4d4bf461a7a0d5d24b6bdca1544c8218eb8a7502
17TheWord <17theword@gmail.com>
2025-10-16 20:53:51 +0800

    :sparkles: 使用官方API实现 Json to Component

--- src/main/java/com/github/theword/queqiao/handle/HandleApiImpl.java
+++ src/main/java/com/github/theword/queqiao/handle/HandleApiImpl.java
@@ -6,44 +6,40 @@ import static com.github.theword.queqiao.utils.ForgeTool.getForgePlayer;
 import java.util.List;
 import java.util.UUID;
 
+import com.github.theword.queqiao.utils.ForgeTool;
+import com.google.gson.JsonElement;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.network.Packet;
 import net.minecraft.network.play.server.S02PacketChat;
-import net.minecraft.util.ChatComponentText;
 
 import com.github.theword.queqiao.tool.handle.HandleApiService;
-import com.github.theword.queqiao.tool.payload.MessageSegment;
-import com.github.theword.queqiao.tool.payload.TitlePayload;
 import com.github.theword.queqiao.tool.response.PrivateMessageResponse;
 import com.github.theword.queqiao.tool.utils.Tool;
-import com.github.theword.queqiao.utils.ParseJsonToEventImpl;
+import net.minecraft.util.IChatComponent;
 
 public class HandleApiImpl implements HandleApiService {
 
-    private final ParseJsonToEventImpl parseJsonToEventImpl = new ParseJsonToEventImpl();
 
     @Override
-    public void handleBroadcastMessage(List<MessageSegment> messageList) {
-        ChatComponentText mutableComponent = parseJsonToEventImpl.parsePerMessageToComponent(Tool.getPrefixComponent());
-        mutableComponent.appendSibling(parseJsonToEventImpl.parseMessageListToComponent(messageList));
-        sendPacket(new S02PacketChat(mutableComponent, true));
+    public void handleBroadcastMessage(JsonElement jsonElement) {
+        IChatComponent component = ForgeTool.buildComponent(Tool.getPrefixComponent());
+        component.appendSibling(ForgeTool.buildComponent(jsonElement));
+        sendPacket(new S02PacketChat(component, true));
     }
 
     @Override
-    public void handleSendTitleMessage(TitlePayload titlePayload) {
-        sendPacket(new S02PacketChat(parseJsonToEventImpl.parseMessageListToComponent(titlePayload.getTitle()), false));
-        if (titlePayload.getSubtitle() != null) sendPacket(
-            new S02PacketChat(parseJsonToEventImpl.parseMessageListToComponent(titlePayload.getSubtitle()), false));
+    public void handleSendTitleMessage(JsonElement titlePayload, JsonElement subTitlePayload, int fadeIn, int stay, int fadeOut) {
+        sendPacket(new S02PacketChat(ForgeTool.buildComponent(titlePayload), false));
+        sendPacket(new S02PacketChat(ForgeTool.buildComponent(subTitlePayload), false));
     }
 
     @Override
-    public void handleSendActionBarMessage(List<MessageSegment> messageList) {
-        sendPacket(new S02PacketChat(parseJsonToEventImpl.parseMessageListToComponent(messageList), false));
+    public void handleSendActionBarMessage(JsonElement jsonElement) {
+        sendPacket(new S02PacketChat(IChatComponent.Serializer.func_150699_a(jsonElement.toString()), false));
     }
 
     @Override
-    public PrivateMessageResponse handleSendPrivateMessage(String nickname, UUID uuid,
-        List<MessageSegment> messageList) {
+    public PrivateMessageResponse handleSendPrivateMessage(String nickname, UUID uuid, JsonElement jsonElement) {
         EntityPlayerMP targetPlayer = null;
         if (uuid != null) {
             List<EntityPlayerMP> playerEntityList = minecraftServer.getConfigurationManager().playerEntityList;
@@ -64,12 +60,12 @@ public class HandleApiImpl implements HandleApiService {
             return PrivateMessageResponse.playerIsNull();
         }
 
-        // if (targetPlayer.field_70135_K) {
-        // return PrivateMessageResponse.playerNotOnline();
-        // }
-        ChatComponentText mutableComponent = parseJsonToEventImpl.parsePerMessageToComponent(Tool.getPrefixComponent());
-        mutableComponent.appendSibling(parseJsonToEventImpl.parseMessageListToComponent(messageList));
-        targetPlayer.addChatComponentMessage(mutableComponent);
+        if (targetPlayer.field_70135_K) {
+            return PrivateMessageResponse.playerNotOnline();
+        }
+        IChatComponent component = ForgeTool.buildComponent(Tool.getPrefixComponent());
+        component.appendSibling(ForgeTool.buildComponent(jsonElement));
+        targetPlayer.addChatComponentMessage(component);
         return PrivateMessageResponse.sendSuccess(getForgePlayer(targetPlayer));
     }
 
