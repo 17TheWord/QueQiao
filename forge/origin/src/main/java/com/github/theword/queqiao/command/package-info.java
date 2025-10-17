f97152554a09917ebd3971b55262819a2c17e3ef
17TheWord <17theword@gmail.com>
2025-10-17 17:22:24 +0800

    :fire: 移除旧版结构

--- forge/origin/src/main/java/com/github/theword/queqiao/command/package-info.java
+++
@@ -1,7 +0,0 @@
-@MethodsReturnNonnullByDefault
-        @ParametersAreNonnullByDefault
-package com.github.theword.queqiao.command;
-
-import mcp.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
