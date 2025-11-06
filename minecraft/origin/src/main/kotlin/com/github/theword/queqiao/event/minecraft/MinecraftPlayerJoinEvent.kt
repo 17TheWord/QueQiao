2eac12b8feb07647ef75b4cf49150396f489a3e5
17TheWord <17theword@gmail.com>
2025-11-06 17:51:14 +0800

    :recycle: 重构事件处理，替换事件为自定义统一事件以增强可读性和可维护性

--- minecraft/origin/src/main/kotlin/com/github/theword/queqiao/event/minecraft/MinecraftPlayerJoinEvent.kt
+++
@@ -1,7 +0,0 @@
-package com.github.theword.queqiao.event.minecraft
-
-import com.github.theword.queqiao.tool.event.base.BasePlayerJoinEvent
-
-data class MinecraftPlayerJoinEvent(
-    private val player: MinecraftPlayer
-) : BasePlayerJoinEvent("MinecraftPlayerJoinEvent", player)
