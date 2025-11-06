6366f4eac782192e39364e67518508bffba13cb3
17TheWord <17theword@gmail.com>
2025-11-06 17:51:11 +0800

    :recycle: 重构事件处理，替换事件为自定义统一事件以增强可读性和可维护性

--- neoforge/origin/src/main/java/com/github/theword/queqiao/event/neoforge/NeoForgeCommandEvent.java
+++
@@ -1,9 +0,0 @@
-package com.github.theword.queqiao.event.neoforge;
-
-import com.github.theword.queqiao.tool.event.base.BaseCommandEvent;
-
-public class NeoForgeCommandEvent extends BaseCommandEvent {
-    public NeoForgeCommandEvent(String messageId, NeoForgeServerPlayer player, String command) {
-        super("NeoCommandEvent", messageId, player, command);
-    }
-}
