14632b2ff0c67b67bccd778088181f3bbc0ba781
17TheWord <17theword@gmail.com>
2025-11-06 17:50:58 +0800

    :recycle: 重构事件处理，替换事件为自定义统一事件以增强可读性和可维护性

--- velocity/origin/src/main/java/com/github/theword/queqiao/event/velocity/VelocityPlayerChatEvent.java
+++
@@ -1,9 +0,0 @@
-package com.github.theword.queqiao.event.velocity;
-
-import com.github.theword.queqiao.tool.event.base.BasePlayerChatEvent;
-
-public class VelocityPlayerChatEvent extends BasePlayerChatEvent {
-    public VelocityPlayerChatEvent(VelocityPlayer player, String message){
-        super("VelocityPlayerChatEvent", "", player, message);
-    }
-}
