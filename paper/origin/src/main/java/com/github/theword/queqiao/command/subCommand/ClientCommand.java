0ac61757d5d66a33b9ee02d17d3d6fda6d78fd02
17TheWord <17theword@gmail.com>
2025-11-11 23:36:57 +0800

    :sparkles: 支持 paper 1.17.1+

---
+++ paper/origin/src/main/java/com/github/theword/queqiao/command/subCommand/ClientCommand.java
@@ -0,0 +1,53 @@
+package com.github.theword.queqiao.command.subCommand;
+
+import com.github.theword.queqiao.command.PaperSubCommand;
+import com.github.theword.queqiao.command.subCommand.client.ReconnectCommand;
+import com.github.theword.queqiao.tool.GlobalContext;
+import com.github.theword.queqiao.tool.command.subCommand.ClientCommandAbstract;
+import org.bukkit.command.CommandSender;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ClientCommand extends ClientCommandAbstract implements PaperSubCommand {
+    @Override
+    public boolean onCommand(CommandSender commandSender, String[] args) {
+        if (!GlobalContext.getHandleCommandReturnMessageService().hasPermission(commandSender, getPermissionNode())) return false;
+        if (args.length == 0) {
+            commandSender.sendMessage("§c请输入子命令");
+            return false;
+        } else if (args[1].equalsIgnoreCase("reconnect")) {
+            return new ReconnectCommand().onCommand(commandSender, args);
+        }
+        return false;
+    }
+
+    @Override
+    public List<String> getSubCommands(CommandSender commandSender, String[] args) {
+        return new ArrayList<String>() {{
+            add("reconnect");
+            add("list");
+        }};
+    }
+
+    @Override
+    public String getPrefix() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return "client";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Websocket Client 的命令";
+    }
+
+    @Override
+    public String getUsage() {
+        return "使用：/mcqq client <args>";
+    }
+}
---
+++ paper/origin/src/main/java/com/github/theword/queqiao/command/subCommand/HelpCommand.java
@@ -0,0 +1,35 @@
+package com.github.theword.queqiao.command.subCommand;
+
+
+import com.github.theword.queqiao.command.CommandManager;
+import com.github.theword.queqiao.command.PaperSubCommand;
+import com.github.theword.queqiao.tool.GlobalContext;
+import com.github.theword.queqiao.tool.command.SubCommand;
+import com.github.theword.queqiao.tool.command.subCommand.HelpCommandAbstract;
+import org.bukkit.command.CommandSender;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelpCommand extends HelpCommandAbstract implements PaperSubCommand {
+    @Override
+    public boolean onCommand(CommandSender commandSender, String[] args) {
+        if (!GlobalContext.getHandleCommandReturnMessageService().hasPermission(commandSender, getPermissionNode())) return false;
+        commandSender.sendMessage("-------------------");
+        for (SubCommand subCommand : new CommandManager().getSubCommandList()) {
+            commandSender.sendMessage(subCommand.getUsage() + "---" + subCommand.getDescription());
+        }
+        commandSender.sendMessage("-------------------");
+        return true;
+    }
+
+    @Override
+    public List<String> getSubCommands(CommandSender commandSender, String[] args) {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public String getPrefix() {
+        return null;
+    }
+}
